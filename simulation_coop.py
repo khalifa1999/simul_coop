d2cdfada4ecd4db96e8980c3c684dea40c804aed
Khalifa Mamadou NIAMADIO <khalifaniamadio@gmail.com>
2022-02-08 20:32:32 +0000

    mise a jour

--- simulation_coop.py
+++ simulation_coop.py
@@ -7,18 +7,7 @@ scrollbar = Scrollbar(root)
 scrollbar.pack( side = RIGHT, fill = Y )
 
 #first part CALCUL DU COUT GLOBAL
-
-def coutGlobal():
-	ca = 0
-	ct = int(ct_entry.get())
-	cc= int(cc_entry.get())
-	ca = int(ca_entry.get())
-	global cg
-	cg = ct+cc+ca
-
-
-	Label(text=f"le cout global {cg}", font='arial 20 bold',  bg='blue').place(x=600,y=150)
-
+# Remove text from label
 
 
 titre_un=Label(root, text="CONSTRUCTION ou TERRAIN + CONSTRUCTION ou ACQUISITION BIEN IMMOBILIER", font="arial 15 bold").place(x=100,y=25)
@@ -31,9 +20,9 @@ cout_terrain.place(x=100,y=50)
 cout_construction.place(x=100,y=100)
 cout_acquis.place(x=100,y=150)
 
-principalValue = StringVar()
-rateValue = StringVar()
-timeValue = StringVar()
+principalValue = StringVar(root, value = 0)
+rateValue = StringVar(root, value = 0)
+timeValue = StringVar(root, value = 0)
 
 ct_entry=Entry(root,textvariable=principalValue, font="arial 20", width=10)
 cc_entry=Entry(root,textvariable=rateValue, font="arial 20", width=10)
@@ -43,7 +32,7 @@ ct_entry.place(x=400,y=50)
 cc_entry.place(x=400,y=100)
 ca_entry.place(x=400,y=160)
 
-Button(text='cout global', font='arial 15', command=coutGlobal).place(x=600,y=100)
+
 
 
 #Apport de l'agent 
@@ -51,6 +40,11 @@ Button(text='cout global', font='arial 15', command=coutGlobal).place(x=600,y=10
 
 
 def coutApport():
+	ct = int(ct_entry.get())
+	cc= int(cc_entry.get())
+	ca = int(ca_entry.get())
+	global raj, rajout, perc, apport_agent, cg
+	cg = ct+cc+ca
 
 	at = int(atValue.get())
 	af= int(afValue.get())
@@ -64,22 +58,41 @@ def coutApport():
 	perc = ((ca*100)/cg)
 	raj= ((40-perc)*cg)/100
 	rajout= ((90-verif)*cg)/100
+	#if((((apport_agent+apport_sonatel+pb)*100)/cg)>=90):
+			#Label(text=f"apport sonatel : {apport_sonatel}", font='arial 20 bold',  bg='orange').place(x=600,y=450)
+			#Label(text=f"apport agent(FINANCEMENT projet) : {apport_agent}", font='arial 20 bold',  bg='grey').place(x=600,y=500)
 	if(((ca*100)/cg)>=40):
-		Label(text=f"l'apport agent {ca}", font='arial 20 bold',  bg='blue').place(x=600,y=350)
-		Label(text=f"Avis favorable de financement", font='arial 20 bold',  bg='green').place(x=600,y=400)
 		if((((apport_agent+apport_sonatel+pb)*100)/cg)>=90):
-			Label(text=f"apport sonatel : {apport_sonatel}", font='arial 20 bold',  bg='orange').place(x=600,y=450)
-			Label(text=f"apport agent(FINANCEMENT projet) : {apport_agent}", font='arial 20 bold',  bg='grey').place(x=600,y=500)
+			if(((apport_agent*100)/cg)>=15):
+				first_case()
+			else:
+				fourth_case()
 		else:
-			Label(text=f"total financement, <90% cout total rajoutez: {rajout}", font='arial 20 bold',  bg='red').place(x=600,y=450)
-	elif(((ca*100)/cg)>=15):
-		Label(text=f"l'apport agent <15% du cout global, rajoutez {((15-perc)*(((apport_agent)*100)/cg))/100}", font='arial 20 bold',  bg='blue').place(x=600,y=350)
+				bis_scd()
 
 	else:
-		Label(text=f"Apport global<40% cout total, rajoutez {raj}", font='arial 20 bold',  bg='red').place(x=600,y=350)
+		fifth_case()
+def first_case():
+	#Label(text=f"l'apport agent {ca}", font='arial 20 bold',  bg='blue').place(x=600,y=350)
+	global text
+	text = Label(root,text=f"Avis favorable de financement.", font='arial 20 bold',  bg='green')
+	text.place(x=100,y=500)
 
+def bis_scd():
+	global text
+	text = Label(root,text=f"financement refusé,total<90%. ", font='arial 20 bold',  bg='red')
+	text.place(x=100,y=500)
+def fourth_case():
+	global text
+	text = Label(root, text=f"financement refusé, apport agent<15% du cout projet.", font='arial 20 bold',  bg='red')
+	text.place(x=100,y=500)
+def fifth_case():
+	global text
+	text = Label(root, text=f"financement refusé, apport global<40% cout projet.", font='arial 20 bold',  bg='red')
+	text.place(x=100,y=500)
 
-
+def remove_text():
+    text.destroy()
 
 titre_DEUX=Label(root, text="Apport agent", font="arial 15 bold").place(x=100,y=200)
 
@@ -95,11 +108,11 @@ pret_banquaire.place(x=100,y=350)
 app_num.place(x=100,y=400)
 app_tr.place(x=100,y=450)
 
-atValue = StringVar()
-afValue = StringVar()
-pbValue = StringVar()
-anValue = StringVar()
-atrValue = StringVar()
+atValue = StringVar(root, value = 0)
+afValue = StringVar(root, value = 0)
+pbValue = StringVar(root, value = 0)
+anValue = StringVar(root, value = 0)
+atrValue = StringVar(root, value = 0)
 
 at_entry=Entry(root,textvariable=atValue, font="arial 20", width=10)
 af_entry=Entry(root,textvariable=afValue, font="arial 20", width=10)
@@ -122,8 +135,9 @@ atr_entry.place(x=400,y=450)
 
 #bouttons
 
-Button(text='cout global', font='arial 15', command=coutGlobal).place(x=600,y=100)
-Button(text='apport agent', font='arial 15', command=coutApport).place(x=600,y=300)
+Button(text='OUTPUT', font='arial 15', command=coutApport).place(x=600,y=200)
+reset = Button(root,text='reset' ,font='arial 15' ,command=remove_text)
+reset.place(x= 700,y=200)
 
 Button(root,text='exit',command=lambda:exit(),font="arial 15",width=0).place(x=400,y=600)
 
